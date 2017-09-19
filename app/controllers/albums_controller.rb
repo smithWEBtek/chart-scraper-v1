c957d440b1a0d1278c304f98bdc2f6da7971b9ea
smithwebtek <brad@smithwebtek.com>
2017-09-19 12:20:18 -0400

    edit albums_controller, remove unneeded views

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,22 +1,50 @@
 class AlbumsController < ApplicationController
-  def index
-  end
 
-  def show
-  end
+	def index
+		@albums = album.all 
+	end
 
-  def new
-  end
+	def show
+		@album = album.find_by_id(params[:id])
+	end
 
-  def create
-  end
+	def new
+		@album = Album.new 
+	end
 
-  def edit
-  end
+	def create
+		@album = Album.new(album_params)
+		if @album.save
+			flash[:message] = 'album created.'
+			redirect_to album_path(@album)
+		else
+			render :new
+		end
+	end
 
-  def update
-  end
+	def edit
+		@album = Album.find_by_id(params[:id])
+	end
 
-  def destroy
-  end
+	def update
+		@album = Album.find_by_id(params[:id])
+		@album.update(album_params)
+		if @album.save
+			flash[:message] = 'album updated.'
+			redirect_to album_path(@album)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@album = Album.find_by_id(params[:id])
+		@album.destroy
+		redirect_to albums_path
+	end
+
+	private
+	def album_params
+		params.require(:album).permit(:name, :location)
+	end
 end
